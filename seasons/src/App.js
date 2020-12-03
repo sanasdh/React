6e689483d584b5048f95752f067df98104a2b2a3
sanasdh <sanasdh@gmail.com>
2020-12-03 15:29:24 -0500

    seasons

---
+++ seasons/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import SeasonDisplay from './SeasonDisplay'
+const App =()=>{
+  window.navigator.geolocation.getCurrentPosition(position=> console.log(position),
+  err=> console.log(err))
+      return(
+<div>
+hiii
+</div>
+    )
+}
+
+export default App;
